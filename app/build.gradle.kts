fad1fe2893982c47cf8e5fe7d5be56005f3758b1
deyvidandrades <deyvid-asilva@hotmail.com>
2024-07-12 12:02:20 -0300

    * Gradle update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,9 +34,9 @@ android {
 
 dependencies {
     //Core libs
-    implementation("androidx.core:core-ktx:1.13.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
 
     //Android widgets
     implementation("androidx.core:core-splashscreen:1.0.1")
@@ -46,7 +46,7 @@ dependencies {
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.2.0-alpha01")
 
     //Outras bibliotecas
-    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.code.gson:gson:2.11.0")
     implementation("com.google.android.play:review:2.0.1")
     implementation("com.google.android.play:review-ktx:2.0.1")
     implementation("com.github.bumptech.glide:glide:4.16.0")
@@ -54,6 +54,6 @@ dependencies {
 
     //Test libraries
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
