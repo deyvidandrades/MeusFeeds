d175466f8a55ed6047c5d5d9ef7491e1fd62c7ea
deyvidandrades <deyvid-asilva@hotmail.com>
2025-02-05 19:13:30 -0300

    * Update: Gradle update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.deyvidandrades.meusfeeds"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.deyvidandrades.meusfeeds"
         minSdk = 31
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 12
         versionName = "1.0.12"
 
@@ -34,14 +34,14 @@ android {
 
 dependencies {
     //Core libs
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
 
     //Android widgets
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.preference:preference-ktx:1.2.1")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
     implementation("com.google.android.flexbox:flexbox:3.0.0")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.2.0-alpha01")
 
@@ -50,7 +50,7 @@ dependencies {
     implementation("com.google.android.play:review:2.0.2")
     implementation("com.google.android.play:review-ktx:2.0.2")
     implementation("com.github.bumptech.glide:glide:4.16.0")
-    implementation("androidx.work:work-runtime-ktx:2.9.1")
+    implementation("androidx.work:work-runtime-ktx:2.10.0")
 
     //Test libraries
     testImplementation("junit:junit:4.13.2")
