5d7e496d0a667cd8f68a1f6c4a8e85b20b457c7e
deyvidandrades <deyvid-asilva@hotmail.com>
2025-04-18 08:32:33 -0300

    * Version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.deyvidandrades.meusfeeds"
         minSdk = 31
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.1.0"
+        versionCode = 15
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
